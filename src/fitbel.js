851c619c5682ed0e135c31b400f5743e1aa57fe1
Denise Fernandez <denise@antena.co>
2016-09-16 15:57:51 -0300

    add refreshToken wrapper, log errors

--- src/fitbel.js
+++ src/fitbel.js
@@ -55,31 +55,68 @@
         },
         function(e, access_token, refresh_token, results) {
           if (e) {
+            console.log("[getOAuthAccessToken] error = ", e);
             reject(e);
           }
           else {
             fitbel.data.user_id = results.user_id;
             fitbel.data.access_token = access_token;
-            resolve(e, access_token, refresh_token, results);
+
+            results.refresh_token = refresh_token;
+            resolve(results);
           }
         }
       )
     });
   }
 
-  function get (query) {
-    var url = FITBIT_API_URL + "1/user/" + fitbel.data.user_id + query;
+  function refreshToken(refreshToken, redirectUri) {
+    var client = this.oauth2;
+    client._baseSite = FITBIT_API_URL;
+    return new Promise(function (resolve, reject) {
+      client.getOAuthAccessToken(
+        refreshToken,
+        {
+          'grant_type': 'refresh_token'
+        },
+        function(e, access_token, refresh_token, results) {
+          if (e) {
+            console.log("[refreshToken] error = ", e);
+            reject(e);
+          }
+          else {
+            fitbel.data.user_id = results.user_id;
+            fitbel.data.access_token = access_token;
+
+            results.refresh_token = refresh_token;
+            resolve(results);
+          }
+        }
+      )
+    });
+  }
+
+  function get(query, userId, token) {
+    var credentials = {
+      userId: userId || fitbel.data.user_id,
+      token: token || fitbel.data.access_token
+    };
+
+    var url = FITBIT_API_URL + "1/user/" + credentials.userId + query;
+
     return new Promise(function (resolve, reject) {
       request({
         url: url,
         headers: {
-          Authorization: 'Bearer ' + fitbel.data.access_token
+          Authorization: 'Bearer ' + credentials.token
         }
       }, function (err, res, body) {
+
         if (!err && res.statusCode === 200) {
           resolve(body);
         } else {
-          reject(body);
+          console.log("[get " + query + "] error = ", err);
+          reject(err);
         }
       })
     });
@@ -88,6 +125,7 @@
   fitbel.connect = connect;
   fitbel.getAuthorizeUrl = getAuthorizeUrl;
   fitbel.getToken = getToken;
+  fitbel.refreshToken = refreshToken;
   fitbel.get = get;
 
   return fitbel;
